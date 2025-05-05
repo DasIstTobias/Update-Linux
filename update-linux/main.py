f8383d118ee159e5881dd5c5be0b8d9db9c1f156
DasIstTobias <139079469+DasIstTobias@users.noreply.github.com>
2025-05-05 20:39:57 +0200

    Added package list refreshing

--- update-linux/main.py
+++ update-linux/main.py
@@ -13,6 +13,7 @@ import threading
 
 # Colour codes
 GREEN = '\033[92m'
+BLUE = '\033[94m'
 RESET = '\033[0m'
 
 
@@ -32,14 +33,36 @@ def _spin_cursor():
 
     while not _spinner_stop_event.is_set():
         char = spinner_chars[i % len(spinner_chars)]
-        sys.stdout.write(f'\rrecognizing distro {char}')
+        sys.stdout.write(f'\rdetecting distro {char}')
         sys.stdout.flush()
         time.sleep(0.1)
         i += 1
 
-    sys.stdout.write('\r' + ' ' * (len("recognizing distro") + 2) + '\r')
+    sys.stdout.write('\r' + ' ' * (len("detecting distro") + 2) + '\r')
     sys.stdout.flush()
     _spinner_running = False
+
+# Spinner animation
+_spinner_2_running = False
+_spinner_2_stop_event = threading.Event()
+
+def _spin_cursor_2():
+    # displays rotating ascii spinner
+    global _spinner_2_running
+    _spinner_2_running = True
+    spinner_2_chars = ['[ | ]', '[ / ]', '[ - ]', '[ \\ ]']
+    i = 0
+
+    while not _spinner_2_stop_event.is_set():
+        char = spinner_2_chars[i % len(spinner_2_chars)]
+        sys.stdout.write(f'\rrefresh package lists {char}')
+        sys.stdout.flush()
+        time.sleep(0.1)
+        i += 1
+
+    sys.stdout.write('\r' + ' ' * (len("refresh package lists") + 2) + '\r')
+    sys.stdout.flush()
+    _spinner_2_running = False
 
 
 ## ------ Productive code ------ ##
@@ -86,7 +109,7 @@ def detecting_distro():
             return None
     
     # Print text
-    sys.stdout.write("recognizing distro ")
+    sys.stdout.write("detecting distro ")
     sys.stdout.flush()
 
     # Start Spinner in new Thread
@@ -127,7 +150,7 @@ def detecting_distro():
         spinner_thread.join(timeout=1)
         # Print green Message
         clear_screen()
-        sys.stdout.write(f"\r{GREEN}recognizing distro [ x ]{RESET}\n")
+        sys.stdout.write(f"\r{GREEN}detecting distro [ x ]{RESET}\n")
         sys.stdout.flush()
 
 
@@ -170,6 +193,7 @@ def program():
     input("[ Press enter to start Update ]")
     clear_screen()
 
+    ## ---- detecting_distro ---- ##
     detecting_distro()
 
     # Create variables
@@ -218,8 +242,76 @@ def program():
         input("[ Press enter to exit program ]")
         sys.exit(1)
 
-    # Write more code here
+    ## ---- Refresh package lists ---- ##
+
+    # Print text
+    print("refresh package lists ", end='')
+    sys.stdout.flush()
+ 
+    # Start Spinner in new Thread
+    _spinner_2_stop_event.clear()
+    spinner_2_thread = threading.Thread(target=_spin_cursor_2, daemon=True)
+    spinner_2_thread.start()
+
+    process_output = ""
+    process_error = ""
+    return_code = 1
     
+    # Refresh package lists
+    if has_apt == True:
+        script_name = 'refresh_repos/apt.sh'
+        script_path = os.path.join(os.path.dirname(__file__), script_name)
+        os.system('chmod +x refresh_repos/apt.sh')
+
+        subprocess.run([script_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
+
+    if has_dnf == True:
+        script_name = 'refresh_repos/dnf.sh'
+        script_path = os.path.join(os.path.dirname(__file__), script_name)
+        os.system('chmod +x refresh_repos/dnf.sh')
+
+        subprocess.run([script_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
+
+    if has_zypper_leap == True:
+        script_name = 'refresh_repos/zypper.sh'
+        script_path = os.path.join(os.path.dirname(__file__), script_name)
+        os.system('chmod +x refresh_repos/zypper.sh')
+
+        subprocess.run([script_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
+
+    if has_zypper_tumbleweed == True:
+        script_name = 'refresh_repos/zypper.sh'
+        script_path = os.path.join(os.path.dirname(__file__), script_name)
+        os.system('chmod +x refresh_repos/zypper.sh')
+
+        subprocess.run([script_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
+
+    if has_pacman == True:
+        script_name = 'refresh_repos/pacman.sh'
+        script_path = os.path.join(os.path.dirname(__file__), script_name)
+        os.system('chmod +x refresh_repos/pacman.sh')
+
+        subprocess.run([script_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
+
+    if has_flatpak == True:
+        script_name = 'refresh_repos/flatpak.sh'
+        script_path = os.path.join(os.path.dirname(__file__), script_name)
+        os.system('chmod +x refresh_repos/flatpak.sh')
+
+        subprocess.run([script_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
+    
+    # Stop Spinner
+        _spinner_2_stop_event.set()
+        spinner_2_thread.join(timeout=1)
+        # Print green Message
+        print(f"\r\r{GREEN}refresh package lists [ x ]{RESET}\n", end='')
+        sys.stdout.flush()
+    
+
+    ## ---- Update Packages ---- ##
+
+    # Write more code here
+
 
 
 # Starts the program when it is executed with sudo permissions
