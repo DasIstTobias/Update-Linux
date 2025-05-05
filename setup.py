8d8cbbb9946579d8575d17c10c22bca485b2334c
DasIstTobias <139079469+DasIstTobias@users.noreply.github.com>
2025-05-05 17:39:29 +0200

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='update-linux',
+    version='1.0',
+    author='tobias@randombytes',
+    author_email='placeholder@example.com',
+    description='Tool to quickly update your linux system',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/DasIstTobias/Update-Linux',
+    packages=find_packages(),
+    install_requires=[
+        # placeholder
+    ],
+    entry_points={
+        'console_scripts': [
+            'update-linux=update-linux.main:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
