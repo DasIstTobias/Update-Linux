63313feb65a223d3990d655b402c3285097c90e1
Tobias <139079469+DasIstTobias@users.noreply.github.com>
2025-05-06 17:46:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,25 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/DasIstTobias/Update-Linux',
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'update_linux': [
+            'detect_distro.sh',
+            'refresh_repos/apt.sh',
+            'refresh_repos/dnf.sh',
+            'refresh_repos/flatpak.sh',
+            'refresh_repos/pacman.sh',
+            'refresh_repos/zypper.sh',
+            'update_packages/apt.sh',
+            'update_packages/dnf.sh',
+            'update_packages/flatpak.sh',
+            'update_packages/pacman.sh',
+            'update_packages/snap.sh',
+            'update_packages/yay.sh',
+            'update_packages/zypper-leap.sh',
+            'update_packages/zypper-tumbleweed.sh',
+        ],
+    },
     install_requires=[
         # placeholder
     ],
